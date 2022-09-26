ccfe4b550533adfbbbfae88ef345b35785205113
Danilo Alcoforado <alcoforado.danilo@gmail.com>
2022-09-25 22:03:35 -0300

    creating the initial structure
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>

---
+++ db/migrate/20220925204450_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[7.0]
+  def change
+    create_table :people do |t|
+      t.string :name, null: false
+      t.string :blood_type
+      t.date :birthdate
+
+      t.timestamps
+    end
+  end
+end
