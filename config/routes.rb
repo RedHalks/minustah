731797b2303387d61913f8211e346319ee9f201c
Danilo Alcoforado <alcoforado.danilo@gmail.com>
2022-10-14 14:58:57 -0300

    add gemfile lock to linux
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "members#new"
+  # root "devise/sessions#new"
 
   resources :cash_flows, except: [:show]
   resources :invoices, except: [:show]
