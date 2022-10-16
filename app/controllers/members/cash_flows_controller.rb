8815eed401fa58a455228bb73938568c96e0b2ed
Danilo Alcoforado <alcoforado.danilo@gmail.com>
2022-10-16 11:47:23 -0300

    fixing cash flow controller
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>

--- app/controllers/members/cash_flows_controller.rb
+++ app/controllers/members/cash_flows_controller.rb
@@ -1,64 +1,66 @@
-class CashFlowsController < AuthenticatedController
-  before_action :find_cash_flow, only: %i[edit update destroy]
+module Members
+  class CashFlowsController < AuthenticatedController
+    before_action :find_cash_flow, only: %i[edit update destroy]
 
-  def index
-    params[:q] ||= {}
-    params[:p] ||= {}
-    params[:s] ||= ['date asc']
+    def index
+      params[:q] ||= {}
+      params[:p] ||= {}
+      params[:s] ||= ['date asc']
 
-    @pagy, @cash_flows = pagy(CashFlow.all, items: 10)
-    # CashFlowInteractor::List.call(params.permit!)
-  end
-
-  def new
-    @cash_flow = CashFlow.new
-  end
-
-  def create
-    result = CashFlowInteractor::Create.call(params: permitted_params)
-
-    if result.success?
-      create_success_notice
-      redirect_to cash_flows_path
-    else
-      @cash_flow = result.cash_flow
-      build_error_alert
-      render :new
+      @pagy, @cash_flows = pagy(CashFlow.all, items: 10)
+      # CashFlowInteractor::List.call(params.permit!)
     end
-  end
 
-  def edit; end
-
-  def update
-    result = CashFlowInteractor::Update.call(cash_flow: @cash_flow, params: permitted_params)
-
-    if result.success?
-      update_success_notice
-      redirect_to cash_flows_path
-    else
-      @cash_flow = result.cash_flow
-      build_error_alert
-      render :edit
+    def new
+      @cash_flow = CashFlow.new
     end
-  end
 
-  def destroy
-    @cash_flow.destroy ? delete_success_notice : build_error_alert
+    def create
+      result = CashFlowInteractor::Create.call(params: permitted_params)
 
-    redirect_to cash_flows_path
-  end
+      if result.success?
+        create_success_notice
+        redirect_to cash_flows_path
+      else
+        @cash_flow = result.cash_flow
+        build_error_alert
+        render :new
+      end
+    end
 
-  private
+    def edit; end
 
-  def build_error_alert
-    flash[:alert] = @cash_flow.errors.map(&:full_message)
-  end
+    def update
+      result = CashFlowInteractor::Update.call(cash_flow: @cash_flow, params: permitted_params)
 
-  def find_cash_flow
-    @cash_flow = CashFlow.find(params[:id])
-  end
+      if result.success?
+        update_success_notice
+        redirect_to cash_flows_path
+      else
+        @cash_flow = result.cash_flow
+        build_error_alert
+        render :edit
+      end
+    end
 
-  def permitted_params
-    params.require(:cash_flow).permit(:category, :kind, :value, :member_id, :moved_at).to_h
+    def destroy
+      @cash_flow.destroy ? delete_success_notice : build_error_alert
+
+      redirect_to cash_flows_path
+    end
+
+    private
+
+    def build_error_alert
+      flash[:alert] = @cash_flow.errors.map(&:full_message)
+    end
+
+    def find_cash_flow
+      @cash_flow = CashFlow.find(params[:id])
+    end
+
+    def permitted_params
+      params.require(:cash_flow).permit(:category, :kind, :value, :member_id, :moved_at).to_h
+    end
   end
 end
