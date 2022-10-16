0bad8d0bf0d5ad1fac55e47a6aee71db9576b897
Danilo Alcoforado <alcoforado.danilo@gmail.com>
2022-10-16 10:28:29 -0300

    config login
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :registerable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :lockable,
          :recoverable, :rememberable, :validatable
 end
