aefaa2add919315c008dbb20753f89801092434c
redhalksAL <114371122+redhalksAL@users.noreply.github.com>
2022-10-23 11:15:20 -0300

    Feature/add report (#9)
    * fixing cash flow controller
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    * adding report
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    * addind paid report
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    * fixing links
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    * rubocop
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    * adding title
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    
    Signed-off-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>
    Co-authored-by: Danilo Alcoforado <alcoforado.danilo@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :welcome do
     get :index
+    get :build_invoices
   end
 
   resources :cash_flows, except: [:show]
@@ -17,5 +18,11 @@ Rails.application.routes.draw do
   resources :members, except: [:show, :destroy] do
     resources :invoices, except: [:show, :destroy], controller: 'members/invoices'
     resources :cash_flows, except: [:show, :destroy], controller: 'members/cash_flows'
+  end
+
+  namespace :reports do
+    namespace :invoices do
+      get :paid
+    end
   end
 end
